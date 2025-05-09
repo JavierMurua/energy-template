1eb2b31d0451c8a9515e345c4c7a85f901393a3e
Javier Murua <javierantoniomurua@gmail.com>
2025-05-09 20:20:00 -0300

    Add docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+  output: 'static',
+  outDir: 'docs',
   site: "https://JavierMurua.github.io",
   base: "/energy-template"
 });
